ddf15f855997d32b14081ffdeb7e55834668ca16
Dosanyintolu <doyin.osanyintolu@gmail.com>
2020-03-22 11:50:24 -0400

    Project name changed

--- MemeMe1.0/ViewController.swift
+++ MemeMe1.0/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     let memeTextAttributes: [NSAttributedString.Key: Any] = [
         NSAttributedString.Key.strokeColor:  UIColor.white,
         NSAttributedString.Key.foregroundColor: UIColor.white,
-        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 26)!,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 22)!,
         NSAttributedString.Key.strokeWidth: 5
     ]
     
